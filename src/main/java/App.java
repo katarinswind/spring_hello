d724b294d271a730ea9a8f6bfbb4238585010b25
wdm <wdm@wdm>
2023-12-19 22:33:36 +0700

    work fine

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean_0 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean_1 = (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean_0.getMessage());
+//        System.out.println(bean_1.getMessage());
+
+        Cat cat_0 = (Cat) applicationContext.getBean("cat");
+        Cat cat_1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.printf("%b\n", bean_0 == bean_1);
+        System.out.printf("%b\n", cat_0 == cat_1);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat Cat = new Cat();
+        Cat.meow();
+        return Cat;
+    }
 }
